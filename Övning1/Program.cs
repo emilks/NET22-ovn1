ff351c1177e1bb15111c40a58640c91d4aca3763
emilks <emil.keski@gmail.com>
2022-05-10 16:36:17 +0200

    Uppdatering

--- Övning1/Program.cs
+++ Övning1/Program.cs
@@ -1,3 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Test");
+﻿/* Uppgift 1 
+ * Klasser:
+ * Employee och main
+ * 
+ * Uppgift 2
+ * Employee har attributerna namn och lön 
+ * Och metoder att skapa en employee och att skriva ut en
+ */
+
+public class Employee
+{
+    public string namn;
+    public double lon;
+    
+    public Employee(string nyttNamn, double nyLon)
+    {
+        namn = nyttNamn;
+        lon = nyLon;
+    }
+
+    public void PrintEmp()
+    {
+        Console.WriteLine("Anställd "+namn+" med lön "+lon);
+    }
+} 
+
+
+class main
+{
+    static void Main()
+    {
+        Console.WriteLine("Välkommen!");
+        bool exit = false;
+        List<Employee> EmployeeList = new List<Employee>();
+        while (!exit)
+        {
+            Console.WriteLine("Vill du lägga till en anställd skriv 1.");
+            Console.WriteLine("Vill se alla anställda skriv 2.");
+            Console.WriteLine("Vill du avsluta skriv 3.");
+
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            if (val == 1){
+                addEmployee(EmployeeList);
+            }else if(val == 2)
+            {
+                listEmployees(EmployeeList);
+            }else if(val == 3)
+            {
+                Console.WriteLine("Hej då");
+                exit = true;
+            }
+            Console.WriteLine("\n");
+        }
+
+    }
+
+    static void addEmployee(List<Employee> EmployeeList)
+    {
+        Console.WriteLine("Skriv namn:");
+        String namn = Console.ReadLine();
+        Console.WriteLine("Skriv lön:");
+        double lon = Convert.ToDouble(Console.ReadLine());
+
+        var anstalld = new Employee(namn, lon);
+        EmployeeList.Add(anstalld);
+        anstalld.PrintEmp();
+
+        return;
+    }
+    static void listEmployees(List<Employee> EmployeeList)
+    {
+        Console.WriteLine("Lista över alla anställda:");
+        foreach(var i in EmployeeList)
+        {
+            i.PrintEmp();
+        }
+        return;
+    }
+}
+
